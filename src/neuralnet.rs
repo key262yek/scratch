325442e87931cda7af90c4b243e2f2d410a64764
Myeongseon Choi <key262yek@gmail.com>
2022-08-25 11:18:28 +0900

    implement activation fns

---
+++ src/neuralnet.rs
@@ -0,0 +1,85 @@
+use ndarray::Array1;
+
+#[allow(dead_code)]
+fn node(x: &Array1<f64>, w: &Array1<f64>, h: &dyn Fn(f64) -> f64) -> f64 {
+    h(x.dot(w))
+}
+
+#[allow(dead_code)]
+fn step_fn(x: f64) -> f64 {
+    if x <= 0.0 {
+        return 0.0;
+    } else {
+        return 1.0;
+    }
+}
+
+#[allow(dead_code)]
+fn step_fn_arr(mut x: Array1<f64>) -> Array1<f64> {
+    x.mapv_inplace(step_fn);
+    x
+}
+
+#[allow(dead_code)]
+fn sigmoid(x: f64) -> f64 {
+    return 1.0 / (1.0 + (-x).exp());
+}
+
+#[allow(dead_code)]
+fn sigmoid_arr(mut x: Array1<f64>) -> Array1<f64> {
+    x.mapv_inplace(sigmoid);
+    x
+}
+
+#[allow(dead_code)]
+fn relu(x: f64) -> f64 {
+    if x <= 0.0 {
+        return 0.0;
+    } else {
+        return x;
+    }
+}
+
+#[allow(dead_code)]
+fn relu_arr(mut x: Array1<f64>) -> Array1<f64> {
+    x.mapv_inplace(relu);
+    x
+}
+
+#[allow(dead_code)]
+fn softmax_arr(mut x: Array1<f64>) -> Array1<f64> {
+    let max = x.fold(f64::NEG_INFINITY, |a, &b| if a < b { b } else { a });
+    x.mapv_inplace(|s| (s - max).exp());
+
+    let sum = x.sum();
+    x.mapv_inplace(|s| s / sum);
+    x
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use approx::assert_abs_diff_eq;
+    use ndarray::array;
+
+    #[test]
+    fn test_sigmoid() {
+        let test = sigmoid_arr(array![-1.0, 1.0, 2.0]);
+        let ans = array![0.26894142, 0.73105858, 0.88079708];
+
+        assert_abs_diff_eq!(test, ans, epsilon = 1e-5);
+    }
+
+    #[test]
+    fn test_softmax() {
+        let test1 = softmax_arr(array![0.3, 2.9, 4.0]);
+        let ans = array![0.01821127, 0.24519181, 0.73659691];
+
+        assert_abs_diff_eq!(test1, ans, epsilon = 1e-5);
+
+        let test2 = softmax_arr(array![1010f64, 1000f64, 990f64]);
+        let ans = array![9.999546e-1, 4.53978686e-5, 2.06106005e-9];
+
+        assert_abs_diff_eq!(test2, ans, epsilon = 1e-5);
+    }
+}
